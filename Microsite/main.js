3df9a96caeceb23837905c410c1cbdfa57d8ba28
temmm741 <temmm741@users.noreply.github.com>
2017-11-09 18:14:07 -0500

    added java

---
+++ Microsite/main.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function(){
+	var documentheight = $(document).height();
+	$(window).scroll(function(){
+	var scrollTop = $(window).scrollTop();
+		console.log (scrollTop)	
+		if(scrollTop > documentheight/4){
+		$("#paragraph").addClass("show")	
+		}
+		if(scrollTop > documentheight/3){
+		$("#paragraph").addClass("notsticky")	
+	
+		}
+
+	})
+})
+
